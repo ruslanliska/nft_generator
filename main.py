bde0516cd43927aee5738126a0dfd21ce20e272a
ruslan.liska <ruslan.liska@globallogic.com>
2022-06-29 17:24:21 +0300

    Modified main file

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from pyfade import Fade, Colors
 
 def main():
     print(Fade.Vertical(Colors.red_to_blue, banner))
-    print("\nСколько изображений сгенерировать?")
+    print("\nHow much NFT's do you want to generate?")
     amount = int(input("-> "))
     for number in range(amount):
         slime_num = number + 1
         create_image(slime_num)
-        print(f"[slime-{slime_num}][{amount}/{slime_num}] - успешно создан")
+        print(f"[slime-{slime_num}][{amount}/{slime_num}] - succesfully created")
 
 
 if __name__ == "__main__":
